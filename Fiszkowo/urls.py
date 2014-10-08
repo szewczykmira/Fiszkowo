a0179d0a8378371755d3fe7043b420bc852d10a4
szewczykmira <szewczyk.mira@gmail.com>
2014-10-08 14:22:37 +0200

    Initial commit
    Webapp created using django, bootrstrap and coffeescript (in future)

---
+++ Fiszkowo/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'Fiszki.views.home', name='home'),
+    url(r'^addcategory/$', 'Fiszki.views.add_category', name='add_category'),
+    url(r'^addfiszka$/$', 'Fiszki.views.edit_fiszka', name='add_fiszka'),
+    url(r'^about/$', 'Fiszki.views.about_author', name='about_author'),
+    url(r'^details/(?P<fiszka_id>\d+)/$', 'Fiszki.views.display_fiszka', name='details'),
+    url(r'^deletecategory/(?P<cat_id>\d+)/$', 'Fiszki.views.delete_category', name='delete_category'),
+    url(r'^deletefiszka/(?P<fiszka_id>\d+)/$', 'Fiszki.views.display_fiszka', name='delete_fiszka'),
+
+    url(r'^admin/', include(admin.site.urls)),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
