423222e1b37d949288bd2d55e1f1b30fd020e00a
szewczykmira <szewczyk.mira@gmail.com>
2014-11-14 20:25:28 +0100

    Prototype ended!

--- dictionary/views.py
+++ dictionary/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.core.urlresolvers import reverse
 
 from .models import Article
+from .forms import ArticleForm
 
 
 def home(request):
@@ -17,4 +18,8 @@ def home(request):
     :param request:
     :return:
     """
-    return render(request, 'dictionary/home.html')
+    ret = {'articles': Article.objects.all(),
+           'form': ArticleForm(request.POST or None)}
+    if request.method == 'POST' and ret['form'].is_valid():
+        ret['form'].save()
+    return render(request, 'dictionary/home.html', ret)
