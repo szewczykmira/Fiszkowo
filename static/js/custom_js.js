3cc7e0cbd803c27fa09d0d45abacae1e31f160cf
szewczykmira <szewczyk.mira@gmail.com>
2015-05-14 18:55:43 +0200

    Console.log deleted, form reset after success, prepend new category not append

--- static/js/custom_js.js
+++ static/js/custom_js.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $('#formSubmit').on('click', function(){
-    console.log('Click');
     var frm = $('#addCat');
     $.ajax({
       type: frm.attr('method'),
@@ -8,7 +7,8 @@ $(document).ready(function(){
       data: frm.serialize(),
       success: function(data){
         html_i = '<li><div class="timeline-image"><img class="img-circle img-responsive" alt=""></div><div class="timeline-panel"><div class="timeline-body"><a href="' + data.n_url +'"><h4 class="subheading">' + data.name +'</h4></a></div></div></li>'; 
-        $('#time').append(html_i);
+        $('#time').prepend(html_i);
+        $('#addCat').restet();
       },
     });
   });
